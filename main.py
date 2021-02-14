3e9f30972b9d5e56dd1eb7f2a8d46caa28858b95
igor9696 <igoreq4@wp.pl>
2021-02-14 18:29:41 +0100

    Improving window capture FPS

--- main.py
+++ main.py
@@ -1,41 +1,28 @@
-# script for autonomous driving in EuroTruckSimulator game
+# script for lane tracking in GTA SA
 
 import cv2 as cv
 import numpy as np
 import os
-import pyautogui
-import win32gui
-from PIL import ImageGrab
+from time import time
+from window_capture import CaptureWindow
 
 
-def grab_screen(window_title=None):
-    """ Grab screen from window with specified title and return image as numpy array """
-    if window_title:
-        hwnd = win32gui.FindWindow(None, window_title)
-        if hwnd:
-            # get coordinates
-            win32gui.SetForegroundWindow(hwnd)
-            x, y, x2, y2 = win32gui.GetClientRect(hwnd)
-            x, y = win32gui.ClientToScreen(hwnd, (x, y))
-            x2, y2 = win32gui.ClientToScreen(hwnd, (x2 - x, y2 - y))
-
-            # grab and process image
-            captured_img = ImageGrab.grab(bbox=(x, y, x2, y2))
-            captured_img = np.array(captured_img)
-            captured_img = cv.cvtColor(captured_img, cv.COLOR_RGB2BGR)
-            return captured_img
-
-    else:
-        print('Window file not found!')
+def process_img(src_img):
+    gray = cv.cvtColor(src_img, cv.COLOR_BGR2GRAY)
+    canny = cv.Canny(gray, 120, 200)
+    return canny
 
 
+game_screen = CaptureWindow('GTA: San Andreas')
 while True:
-    img = grab_screen(window_title='Euro Truck Simulator 2')
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    loop_time = time()
+    img = game_screen.capture()
+    res = process_img(img)
+    print('FPS:{}'.format(1 / (time()-loop_time)))
 
-    cv.imshow('Game window', gray)
-    if cv.waitKey(5) & 0xFF == 27:
+    cv.imshow('Result', res)
+    if cv.waitKey(1) == 27:
         break
 
-
 cv.destroyAllWindows()
+
