0d4b996356e1d5e69627d76e1264fd825b88c93f
igor9696 <igoreq4@wp.pl>
2021-02-22 17:27:05 +0100

    first_working_prototype

--- main.py
+++ main.py
@@ -1,47 +1,32 @@
 # script for lane tracking in GTA SA
 
 import cv2 as cv
-import numpy as np
 import utilis
 from time import time, sleep
 from window_capture import CaptureWindow
-from game_control import press_key, release_key, W, A, S, D
-import pydirectinput
 
+GAME_NAME = 'GTA: San Andreas'
+game_screen = CaptureWindow(GAME_NAME)
 
-# # count to 4
-# for i in list(range(4)):
-#     print(i+1)
-#     sleep(1)
-
-# print('press W')
-# pydirectinput.keyDown('w')
-# sleep(2)
-# pydirectinput.keyUp('w')
-# print('press S')
-# pydirectinput.keyDown('s')
-# sleep(2)
-# pydirectinput.keyUp('s')
-
-game_screen = CaptureWindow('GTA: San Andreas')
-
-# img = game_screen.capture()
-# res = utilis.process_img(img)
-# cv.imshow('Result', res)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
+# count to 4 before launch algorithm
+for i in list(range(4)):
+    print(i+1)
+    sleep(1)
 
 
 def main():
     while True:
-        loop_time = time()
+        # loop_time = time()
         img = game_screen.capture()
-        res = utilis.process_img(img)
-        print('FPS:{}'.format(1 / (time() - loop_time)))
+        output_img, lines = utilis.process_img(img)
+        utilis.steering_vehicle(output_img, lines)
+        # print('FPS:{}'.format(1 / (time() - loop_time)))
 
-        cv.imshow('Result', res)
+        cv.imshow('Output', output_img)
         if cv.waitKey(1) == 27:
             break
+
     cv.destroyAllWindows()
+
 
 main()
--- utilis.py
+++ utilis.py
@@ -1,50 +1,79 @@
 import numpy as np
 import cv2 as cv
+from game_control import *
 
 
-def draw_lines(image, lines):
-    if lines is not None:
-        for i in range(0, len(lines)):
-            line = lines[i][0]
-            # cv.line(image, (line[0], line[1]), (line[2], line[3]), (0, 0, 255), 3, cv.LINE_AA)
-    return image
+def get_lines_center(image, line_1, line_2):
+    try:
+        x_l1, y_l1, x_l2, y_l2 = line_1
+        x_r1, y_r1, x_r2, y_r2 = line_2
+
+        left_slope = (y_l2 - y_l1) / (x_l2 - x_l1)
+        right_slope = (y_r2 - y_r1) / (x_r2 - x_r1)
+
+        # find mid X point both left and right line
+        center_lx = (x_l2 - x_l1) / 2 + x_l1
+        center_rx = (x_r2 - x_r1) / 2 + x_r1
+        lanes_cnt = int(center_rx - (center_rx - center_lx) / 2)
+
+        cv.circle(image, (int(center_lx), 400), 10, [255, 0, 0], -1)
+        cv.circle(image, (int(center_rx), 400), 10, [255, 0, 0], -1)
+        cv.circle(image, (lanes_cnt, 400), 10, [255, 255, 0], -1)
+
+        return lanes_cnt, center_lx, center_rx
+
+    except Exception:
+        pass
 
 
-def draw_avg_line(image, lines):
+def calc_avg_line(lines):
     left_line = []
     right_line = []
 
-    if lines is not None:
-        for i in range(0, len(lines)):
-            line = lines[i][0]
-            x1, y1, x2, y2 = line
-            # calc line slope; slope = y2-y1/x2-x1
-            slope = (y2 - y1)/(x2 - x1)
-            if slope == 0:
-                continue
-            elif slope > 0:
-                left_line.append([x1, y1, x2, y2])
-            elif slope < 0:
-                right_line.append([x1, y1, x2, y2])
+    try:
+        # calculate average left and right line
+        if lines is not None:
+            for i in range(0, len(lines)):
+                line = lines[i][0]
+                x1, y1, x2, y2 = line
+                # calc line slope; slope = y2-y1/x2-x1
+                slope = (y2 - y1) / (x2 - x1)
+                if slope == 0:
+                    continue
+                elif slope < 0:
+                    left_line.append([x1, y1, x2, y2])
+                elif slope > 0:
+                    right_line.append([x1, y1, x2, y2])
 
-        # print('left line: ', left_line)
-        # print('right line', right_line)
+            left_avg = np.int16(np.average(left_line, axis=0))
+            right_avg = np.int16(np.average(right_line, axis=0))
 
-        left_avg = np.int16(np.average(left_line, axis=0))
-        right_avg = np.int16(np.average(right_line, axis=0))
+            return left_avg, right_avg
 
-        # print('L AVG', left_avg)
-        # print('R AVG', right_avg)
+    except Exception:
+        pass
 
-        # draw left and right average line
-        try:
-            cv.line(image, (left_avg[0], left_avg[1]), (left_avg[2], left_avg[3]), [0, 255, 0], 4)
-            cv.line(image, (right_avg[0], right_avg[1]), (right_avg[2], right_avg[3]), [0, 255, 0], 4)
 
-        except Exception:
-            pass
+def draw_lines(image, lines):
 
-        return image
+    try:
+        left_avg, right_avg = calc_avg_line(lines)
+
+        # draw left,right average line and central point
+        cv.line(image, (left_avg[0], left_avg[1]), (left_avg[2], left_avg[3]), [0, 255, 0], 6, cv.LINE_AA)
+        cv.line(image, (right_avg[0], right_avg[1]), (right_avg[2], right_avg[3]), [0, 255, 0], 6, cv.LINE_AA)
+
+        # draw fix central image line
+        draw_image_cnt_line(image)
+
+        # get center point between lines and draw edge points between two of them
+        lanes_cnt, center_lx, center_rx = get_lines_center(image, left_avg, right_avg)
+        cv.circle(image, (int(center_lx), 400), 10, [255, 0, 0], -1)
+        cv.circle(image, (int(center_rx), 400), 10, [255, 0, 0], -1)
+        cv.circle(image, (lanes_cnt, 400), 10, [255, 255, 0], -1)
+
+    except Exception:
+        pass
 
 
 def roi(image, vertices):
@@ -54,15 +83,58 @@ def roi(image, vertices):
     return roi_img
 
 
+def draw_image_cnt_line(image):
+    w, h = image.shape[1], image.shape[0]
+    # cv.circle(image, (int(w/2), int(h/2 + 100)), 10, [0, 0, 255], -1)
+    cv.line(image, (int(w/2), int(h/2)),(int(w/2), int(h/2) + 100), [0, 0, 255], 4, cv.LINE_AA)
+    return image
+
+
+def auto_canny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensity
+    median = np.median(image)
+    # apply automatic canny edge detection
+    lower = int(max(0, (1.0 - sigma) * median))
+    upper = int(min(255, (1.0 - sigma) * median))
+    canny = cv.Canny(image, lower, upper)
+
+    return canny
+
+
 def process_img(src_img):
     gray = cv.cvtColor(src_img, cv.COLOR_BGR2GRAY)
-    blur = cv.GaussianBlur(gray, (3, 3), 0)
-    canny = cv.Canny(blur, 50, 130)
+    blur = cv.GaussianBlur(gray, (5, 5), 0)
+    canny = auto_canny(blur)
 
-    vertices = np.array([[350, 300], [450, 300], [800, 550], [0, 550]])
+    vertices = np.array([[250, 350], [600, 350], [800, 600], [0, 600]])
     roi_img = roi(canny, [vertices])
+    lines = cv.HoughLinesP(roi_img, 1, np.pi/180, threshold=100, minLineLength=100, maxLineGap=20)
+    draw_lines(src_img, lines)
 
-    lines = cv.HoughLinesP(roi_img, 1, np.pi/180, 100, minLineLength=120, maxLineGap=5)
-    output_img = draw_lines(src_img, lines)
-    draw_avg_line(output_img, lines)
-    return output_img
+    return src_img, lines
+
+
+def steering_vehicle(image, lines):
+    fixed_central = image.shape[1] / 2
+    # steering
+
+    try:
+        # steering
+        left_avg, right_avg = calc_avg_line(lines)
+        lanes_cnt, center_lx, center_rx = get_lines_center(image, left_avg, right_avg)
+        print('lanes cnt:{0}, fixed_central: {1}'.format(lanes_cnt, fixed_central))
+
+        if (fixed_central < lanes_cnt - 30) and (fixed_central > lanes_cnt - 80):
+            turn_right()
+
+        elif (fixed_central > lanes_cnt + 30) and (fixed_central < lanes_cnt + 80):
+            turn_left()
+
+        elif (fixed_central > lanes_cnt - 50) and (fixed_central < lanes_cnt + 50):
+            move_forward()
+
+        else:
+            slow_down()
+
+    except Exception:
+        slow_down()
